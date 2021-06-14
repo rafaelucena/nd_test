9f6382d1614b406a2199c59a6bed1fb56e44f79e
rboszko <rafa@hotmail.sk>
2021-06-14 22:59:39 +0200

    003 (fix) - refactor code (controllers)

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -6,40 +6,52 @@ namespace App\Controller;
 
 use App\Entity\Item;
 use App\Service\ItemService;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
 {
     /**
      * @Route("/item", name="item_list", methods={"GET"})
      * @IsGranted("ROLE_USER")
+     *
+     * @return JsonResponse
      */
     public function list(): JsonResponse
     {
-        $items = $this->getDoctrine()->getRepository(Item::class)->findBy(['user' => $this->getUser()]);
+        $items = $this
+            ->getDoctrine()
+            ->getRepository(Item::class)
+            ->findBy([
+                'user' => $this->getUser()
+            ]);
 
-        $allItems = [];
+        $itemsList = [];
         foreach ($items as $item) {
             $oneItem['id'] = $item->getId();
             $oneItem['data'] = $item->getData();
             $oneItem['created_at'] = $item->getCreatedAt();
             $oneItem['updated_at'] = $item->getUpdatedAt();
-            $allItems[] = $oneItem;
+            $itemsList[] = $oneItem;
         }
 
-        return $this->json($allItems);
+        return new JsonResponse($itemsList);
     }
 
     /**
      * @Route("/item", name="item_create", methods={"POST"})
      * @IsGranted("ROLE_USER")
+     *
+     * @param Request $request
+     * @param ItemService $itemService
+     *
+     * @return JsonResponse
      */
-    public function create(Request $request, ItemService $itemService)
+    public function create(Request $request, ItemService $itemService): JsonResponse
     {
         $data = $request->get('data');
 
@@ -49,14 +61,16 @@ class ItemController extends AbstractController
 
         $itemService->create($this->getUser(), $data);
 
-        return $this->json([]);
+        return new JsonResponse([]);
     }
 
     /**
      * @Route("/item", name="items_update", methods={"PUT"})
      * @IsGranted("ROLE_USER")
+     *
+     * @return JsonResponse
      */
-    public function update()
+    public function update(): JsonResponse
     {
         $id = put_request('id');
         if (empty($id)) {
@@ -78,14 +92,18 @@ class ItemController extends AbstractController
         $manager = $this->getDoctrine()->getManager();
         $manager->flush();
 
-        return $this->json([]);
+        return new JsonResponse([]);
     }
 
     /**
      * @Route("/item/{id}", name="items_delete", methods={"DELETE"})
      * @IsGranted("ROLE_USER")
+     *
+     * @param int $id
+     *
+     * @return JsonResponse
      */
-    public function delete(Request $request, int $id)
+    public function delete(int $id): JsonResponse
     {
         if (empty($id)) {
             return $this->json(['error' => 'No data parameter'], Response::HTTP_BAD_REQUEST);
@@ -101,6 +119,6 @@ class ItemController extends AbstractController
         $manager->remove($item);
         $manager->flush();
 
-        return $this->json([]);
+        return new JsonResponse([]);
     }
 }
