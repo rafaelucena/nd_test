24c51c0ee4b5763c24754fee3dcbf7c4652a2a80
rboszko <rafa@hotmail.sk>
2021-06-14 23:04:00 +0200

    003 (fix) - refactor code (services)

--- src/Service/ItemService.php
+++ src/Service/ItemService.php
@@ -5,16 +5,29 @@ namespace App\Service;
 use App\Entity\Item;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-
 class ItemService
 {
+    /**
+     * @var EntityManager
+     */
     private $entityManager;
 
+    /**
+     * @param EntityManagerInterface $entityManager
+     *
+     * @return void
+     */
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
 
+    /**
+     * @param User $user
+     * @param string $data
+     *
+     * @return void
+     */
     public function create(User $user, string $data): void
     {
         $item = new Item();
@@ -24,4 +37,4 @@ class ItemService
         $this->entityManager->persist($item);
         $this->entityManager->flush();
     }
-} 
+}
